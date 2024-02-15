c0ac34fb12632d2c297eb640f0c79b3b6e464c3a
Keizerbub <101190682+Keizerbub@users.noreply.github.com>
2024-02-15 18:47:38 +0100

    Update wrangling.py

--- wrangling.py
+++ wrangling.py
@@ -2,7 +2,11 @@ from WeatherDataCollectionModule import AggregationDataset
 
 ag=AggregationDataset()
 
+#folder containing our zipped file
 input_folder=r"C:\Users\Regis Likassi\Documents\AIVANCITY\3e année\clinique IA\weather\test_echantillon"
+
+#final folder in wich you will have the final_departement generated in
 output_folder=r"C:\Users\Regis Likassi\Documents\AIVANCITY\3e année\clinique IA\weather\test_resultat_echantillon"
 
+#the function to decompress and aggregate all file by departement  while removing the original file to not have a storage problem
 ag.decompress_move_and_delete_zip(input_folder, output_folder)
